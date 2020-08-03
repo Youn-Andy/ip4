5a7ea0ed08af5613d13e42674a540a94c201142e
andrew <ayietaphilip00@outlook.com>
2020-08-03 09:17:42 +0300

    Add a prototype function

--- js/script.js
+++ js/script.js
@@ -5,3 +5,9 @@ function Pizza(size, toppings1, toppings2, toppings3, toppings4) {
     this.toppings3 = toppings3;
     this.toppings4 = toppings4;
 };
+
+Pizza.prototype.price = function () {
+    var total = this.toppings1 + this.toppings2 + this.toppings3 + this.toppings4 + this.size;
+    return total;
+};
+
