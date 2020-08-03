7ca409b673a9f46f4d50685fe9e6f44c9941c8d6
andrew <ayietaphilip00@outlook.com>
2020-08-03 09:16:04 +0300

    Add a function using constructors

---
+++ js/script.js
@@ -0,0 +1,7 @@
+function Pizza(size, toppings1, toppings2, toppings3, toppings4) {
+    this.size = size;
+    this.toppings1 = toppings1;
+    this.toppings2 = toppings2;
+    this.toppings3 = toppings3;
+    this.toppings4 = toppings4;
+};
