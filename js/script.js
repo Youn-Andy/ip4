056b96282a3e192e0cf93438e786d4e2b8c2951a
andrew <ayietaphilip00@outlook.com>
2020-08-03 12:43:06 +0300

    Made a few corrections to my html file

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
-function Pizza(size, toppings1, toppings2, toppings3, toppings4) {
+function Pizza(size, crust, toppings1, toppings2, toppings3, toppings4) {
     this.size = size;
+    this.crust = crust;
     this.toppings1 = toppings1;
     this.toppings2 = toppings2;
     this.toppings3 = toppings3;
@@ -7,7 +8,7 @@ function Pizza(size, toppings1, toppings2, toppings3, toppings4) {
 };
 
 Pizza.prototype.price = function () {
-    var total = this.toppings1 + this.toppings2 + this.toppings3 + this.toppings4 + this.size;
+    var total = this.toppings1 + this.toppings2 + this.toppings3 + this.toppings4 + this.size + this.crust;
     return total;
 };
 
@@ -15,6 +16,7 @@ $(document).ready(function () {
     $("#pizzaOrder").submit(function (event) {
         event.preventDefault();
         var size = parseFloat($("#size").val());
+        var crust = parseFloat($("#crust").val());
         var toppings1 = parseFloat($("#toppings1").val());
         var toppings2 = parseFloat($("#toppings2").val());
         var toppings3 = parseFloat($("#toppings3").val());
